5c9e9acbde8c4a92e82ae0be5568eb70e66a0e17
hamidamaqsoudi <hamidamaqsoudi@users.noreply.github.com>
2022-07-27 08:53:27 -0500

    update

---
+++ ascii.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+$("#Start").click(function(){
+
+    $("body>div>fieldset").each(function(index,element){
+        
+        let all= $(element);
+        let field2  = $("element option").html();
+        console.log(field2);
+        
+    })
+
+
+    // let field3 = $()
+        //console.log($(element).html());
+    // let anim = $("div>fieldset>select>option").html();
+    // console.log(anim==="Blank")
+    // if(anim==="Bike"){
+    //     alert("correct");
+    // }
+    // txtArea.value=  ANIMATIONS["Exercise"];
+
+
+});
+
+
+});
---
+++ jstest.js
@@ -0,0 +1,10 @@
+// const body = document.body;
+// const button = document.createElement('button');
+// const p = document.createElement('p');
+// body.append(button);
+// body.append(p);
+
+function ma(){
+    document.getElementById("demo").style.color="blue";
+    document.getElementById("demo").style.fontSize="40px";
+}

fb9183e7adf123b0fdf59fd9e188d63fb255046b
hamidamaqsoudi <hamidamaqsoudi@users.noreply.github.com>
2022-08-01 09:54:25 -0500

    Update decorateMyText.js

--- js/decorateMyText.js
+++ js/decorateMyText.js
@@ -1,14 +1,14 @@
 window.onload = function() {
-  document. querySelector("#bigDecoration"). addEventListener("click", increaseFontReguraly);
-  document. querySelector("#bling"). addEventListener("change", decTextArea);
+  document.querySelector("#bigDecoration").addEventListener("click", increaseFontReguraly);
+  document.querySelector("#bling").addEventListener("change", decTextArea);
 }
 
 
 function increaseFont() {
-  var el = document. getElementById("myText");
-  var style = window. getComputedStyle(el, null). getPropertyValue('font-size');
+  var el= document.getElementById("myText");
+  var style= window.getComputedStyle(el, null).getPropertyValue('font-size');
   var fontSize = parseFloat(style);
-  el. style. fontSize = (fontSize + 2) + 'px';
+  el.style.fontSize = (fontSize + 2) + 'px';
 }
 
 function increaseFontReguraly(){
@@ -16,17 +16,17 @@ function increaseFontReguraly(){
 }
 
 function decTextArea() {
-  if (bling. checked) {
-    myText. style. fontWeight = "bold";
-    myText. style. color = "green";
-    myText. style. textDecoration = "underline";
-    body. style. backgroundImage = "url('https://upload.wikimedia.org/wikipedia/commons/thumb/4/4d/Usdollar100front.jpg/800px-Usdollar100front.jpg?20121125073311')";
+  if (bling.checked) {
+    myText.style.fontWeight="bold";
+    myText.style.color="green";
+    myText.style.textDecoration="underline";
+    body.style.backgroundImage="url('https://upload.wikimedia.org/wikipedia/commons/thumb/4/4d/Usdollar100front.jpg/800px-Usdollar100front.jpg?20121125073311')";
 
   } else {
-    myText. style. fontWeight = "normal";
-    myText. style. color = "black";
-    myText. style. textDecoration = "none";
-    body. style. backgroundImage ="none";
+    myText.style.fontWeight="normal";
+    myText.style.color="black";
+    myText.style.textDecoration="none";
+    body.style.backgroundImage="none";
 
   }
 }
